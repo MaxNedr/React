637ddfdc9f530f983ec0432f83c99153a157bb6b
NedrNout <maxim.nedozrelov@ya.ru>
2019-02-18 22:42:02 +0300

    сборка проходит

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,67 @@
+const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
-    entry: { main: path.resolve(__dirname, 'src', 'index.js') },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+    entry: {
+        app: './index.jsx',
     },
+    context: `${__dirname}/static_src`,
+    output: {
+        path: `${__dirname}/static`,
+        filename: NODE_ENV === 'development' ? '[name].js' : '[name]-[hash].js',
+    },
+
+    optimization: {
+        minimize: NODE_ENV !== 'development',
+    },
+
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new ExtractTextPlugin({ filename: 'style.css' }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'static_src', 'index.html'),
+            filename: 'index.html',
+        })
+    ],
+
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
+                // include: `${__dirname}/static_src`,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: 'babel-loader'
                 }
             },
             {
                 test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader']
-                })
-            }
-        ]
+                loader: 'style-loader!css-loader',
+            },
+            {
+                test: /\.(scss|sass)$/,
+                loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded,modules: true',
+            },
+            {
+                test: /\.(png|jpg|gif|svg|ttf|eot|woff|woff2)$/,
+                loader: 'url-loader?limit=4096&name=[path][name].[ext]',
+            },
+        ],
     },
-    plugins: [
-        new ExtractTextPlugin({ filename: 'style.css' }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-            filename: 'index.html',
-        })
-    ]
+
+    resolve: {
+        modules: [`${__dirname}/static_src`, 'node_modules'],
+        extensions: ['.js', '.jsx'],
+    },
+
+    watch: NODE_ENV === 'development',
+    watchOptions: {
+        aggregateTimeout: 100,
+    },
+
+    devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : false,
 };
